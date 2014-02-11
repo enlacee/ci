cb5801dfc2b9f84b5e1743ef866b9f52e430c713
Anibal Copitan <acopitan@gmail.com>
2014-02-10 23:04:47 -0500

    configuracion basica ci 2.1.4

---
+++ application/views/home/index.php
@@ -0,0 +1,3 @@
+<?php
+echo "<h1>home/index.php</h1>";
+
---
+++ application/views/welcome/hola.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$a = 01;
+$b = 02;
+if ($a ==  $b) {
+    echo "VERDADERO<BR>";
+} else {
+    echo "FALSO<BR>";
+}
+echo "<h1>welcome.php</h1>";

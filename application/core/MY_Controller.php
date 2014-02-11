370b11c878ef6aea580d0ae8bb733eb11b1be96f
Anibal Copitan <acopitan@gmail.com>
2014-02-11 01:33:12 -0500

    libreria : MY_Controlador

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,7 +1,13 @@
 <?php
+/**
+ * @author Anibal Copitan <acopitan@gmail.com>
+ * @category Libreria
+ * 
+ * Encapsulamiento de funciones que se cargan al iniciar el controlador
+ * base CI_Controller ejm : auth, cron, check, etc..
+ */
 
 class MY_Controller extends CI_Controller {
-    //put your code here
     
     public function __construct()
     {
